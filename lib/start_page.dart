e94e62fdfab5ef87da495b929037ed6bbfca205b
swj <swjtwin@naver.com>
2024-06-15 12:37:23 +0900

    feat: pub Getx Add

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -1,12 +1,13 @@
 import 'package:epson_app/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 final class StartPage extends StatelessWidget {
   const StartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const GetMaterialApp(
       home: HomePage(),
     );
   }
